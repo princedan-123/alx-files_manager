a0d55071bc4d8b4dfaee306d2a650da479c04e2b
Uko Uwatt <uwattuko@gmail.com>
2024-08-31 05:59:44 +0100

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,50 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = true;
+
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+
+    this.client.on('error', (error) => {
+      console.error(`${error}`);
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply === null ? null : reply);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
