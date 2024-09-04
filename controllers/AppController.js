78a59ff9020cd59ee1de19a1fc1ec37d9621f55b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-09-04 16:00:50 +0100

    fixed lint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,16 +6,17 @@ const AppController = {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
     if (redisStatus && dbStatus) {
-      return res.status(200).json({ redis: true, db: true })
+      return res.status(200).json({ redis: true, db: true });
     }
+    return null;
   },
   async getStats(req, res) {
     const nbUsers = await dbClient.nbUsers();
     const nbFiles = await dbClient.nbFiles();
-    console.log(nbUsers)
     if (dbClient.isAlive()) {
-      return res.status(200).json({ users: nbUsers, files: nbFiles })
+      return res.status(200).json({ users: nbUsers, files: nbFiles });
     }
+    return null;
   },
 };
 export default AppController;
